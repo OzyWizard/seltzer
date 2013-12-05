89da0e60be6d9dcd135cbe39289e0552d6fdca6c
Buzz <davidbuzz@gmail.com>
2013-12-05 19:57:28 +1000

    push the config file/s for this.  ( except the mysql username and passwrod ).     Yes, I've pushed the identical file twice….. as I'm not sure which one is the right place , so I've put it in both.

---
+++ crm/include/config.inc.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+    Copyright 2009-2013 Edward L. Platt <ed@elplatt.com>
+    
+    This file is part of the Seltzer CRM Project
+    config.inc.php - Sample configuration
+
+    Seltzer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    Seltzer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Seltzer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Database configuration
+$config_db_host = 'localhost';
+$config_db_user = 'xxxxxxxxx';
+$config_db_password = 'xxxxxxxx';
+$config_db_db = 'xxxxxxxx';
+
+// Site info
+
+// The title to display in the title bar
+$config_site_title = 'HSBNE.org CRM';
+
+// The name of the organization to insert into templates
+$config_org_name = 'HSBNE.org CRM';
+
+// The currency code for dealing with payments, can be GBP, USD, or EUR 
+$config_currency_code = 'USD';
+
+// The From: address to use when sending email to members
+$config_email_from = '';
+
+// The email address to notify when a user is created
+$config_email_to = '';
+
+// The hostname of the server
+$config_host = $_SERVER['SERVER_NAME'];
+
+// The url path of the crm directory
+$config_base_path = '/seltzer/crm/';
+
+// The name of the theme you want to use
+// (currently there is only one, "inspire".)
+$config_theme = "inspire";
+
+// Amazon signatures version 2 keys
+$config_amazon_payment_secret = '';
+$config_amazon_payment_access_key_id = '';
+
+// Base modules
+$config_modules = array(
+    "contact",
+    "user",
+    "variable",
+    "member",
+);
+
+// Optional modules, uncomment to enable
+
+// Track RFID key serial numbers
+$config_modules[] = "key";
+
+// Track payments, manual entry
+$config_modules[] = "payment";
+
+// Amazon payment integration
+$config_modules[] = "amazon_payment";
+
+// Paypal integration
+$config_modules[] = "paypal_payment";
+
+// Paypal integration
+$config_modules[] = "westpac_payment";
+
+// Automated billing
+$config_modules[] = "billing";
+
+// Assign members a mentor 
+//$config_modules[] = "mentor";
+
+// Developer tools
+$config_modules[] = "devel";
+
+// meta tags for users:
+$config_modules[] = "user_meta";
+// meta tags for plan:
+$config_modules[] = "plan_meta";
+
+// Links to show in the main menu
+$config_links = array(
+    '<front>' => 'Home'
+    , 'members' => 'Members'
+    , 'plans' => 'Plans'
+    , 'keys' => 'Keys'
+    , 'user_metas' => 'User MetaData'
+    , 'plan_metas' => 'Plan MetaData'
+    , 'payments' => 'Payments'
+    , 'accounts' => 'Accounts'
+    , 'reports' => 'Reports'
+    , 'permissions' => 'Permissions'
+    , 'upgrade' => 'Upgrade'
+);
